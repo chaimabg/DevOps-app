9055dd5e00b2230c6ad973f57c4eb4547cee9661
Chaima <chaimabenghanem@insat.u-carthage.tn>
2022-04-22 14:56:47 +0000

    add pipline

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+var app = require('app');
+const port = 3000;
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+app.listen(port, ()=>{
+  console.log(`app listening on port ${port}`);
+})
 module.exports = router;
