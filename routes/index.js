9951ad7456c5501e9f018146f5ca561b1b6afc63
chaimabg <bghanem.chaima@gmail.com>
2022-06-07 13:16:44 +0000

    adding a service to aws

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,5 @@ const port = 3000;
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-app.listen(port, ()=>{
-  console.log(`app listening on port ${port}`);
-})
+
 module.exports = router;
